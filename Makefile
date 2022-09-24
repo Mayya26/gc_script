8e5cf60d14984c983b30d7ffa25b99bfac0c2c9d
Pchelka <pchelka@pop-os.localdomain>
2022-09-24 16:19:26 +0300

    function for file with setting

--- Makefile
+++ Makefile
@@ -2,3 +2,5 @@ build:
 	gcc -std=c11 script.c
 	./a.out namdtoff
 
+clean:
+	rm -f ./a.out
--- script.c
+++ script.c
@@ -4,16 +4,21 @@
 
 //исправить потом, что он умеет считывать только элекменты из одного символа
 
+
+//структура содержит массив координат и зарядов. В отдельном массиве хранятся символы элементов
+//а так же количество точек км/мм
 typedef struct MATRIX
 {
   double **input;
   int gc, mm;
   char *name;
+  char set[20][100];
 } matrix_t;
 
 matrix_t create(int rows, int columns);
 void remove_matrix(matrix_t *A, int sum);
 void print_matrix(matrix_t data);
+void setting(matrix_t *A);
 
 
 
@@ -30,12 +35,12 @@ int main(int argc, char *argv[]) {
   for (size_t i = n; i < m+n; ++i) {
     fscanf(input, "%lf %lf %lf %lf", &data.input[i][0], &data.input[i][1], &data.input[i][2], &data.input[i][3]);
   }
-  // print_matrix(data);
   fclose(input);
   remove_matrix(&data, n+m);
   return 0;
 }
 
+//просто выводит в терминал то что есть в структуре (кроме настроек)
 void print_matrix(matrix_t data) {
     for (size_t i = 0; i < data.gc+data.mm; ++i) {
     if (i < data.gc) printf("%f %f %f %c\n", data.input[i][0], data.input[i][1], data.input[i][2], data.name[i]);
@@ -43,6 +48,7 @@ void print_matrix(matrix_t data) {
   }
 }
 
+//выделяем функцию под структуру + вызов функции для настроек
 matrix_t create(int rows, int columns) {
   matrix_t res;
   if (rows > 0 && columns > 0) {
@@ -53,11 +59,12 @@ matrix_t create(int rows, int columns) {
     res.name = (char *)calloc(rows, sizeof(char));
     res.gc = rows;
     res.mm = columns;
-  } 
+  }
+  setting(&res);
   return res;
 }
 
-
+//очистка структуры
 void remove_matrix(matrix_t *A, int sum) {
   if (A != NULL) {
       for (int i = 0; i < sum; i++) {
@@ -70,3 +77,28 @@ void remove_matrix(matrix_t *A, int sum) {
   A->mm = 0;
 }
 
+
+//запись из файла настроек в структуру
+void setting(matrix_t *A) {
+  FILE *input;
+  input = fopen("settings.txt", "r");
+  size_t i = 0, k = 0;
+  while(!feof(input)) {
+    A->set[i][0] = fgetc(input);
+    k = 0;
+    if (A->set[i][0] == 'e') {
+      A->set[i][1] = fgetc(input);
+      if (A->set[i][1] == 'n') {
+        A->set[i][2] = fgetc(input);
+        if (A->set[i][2] == 'd') break;
+      }
+      k = 2;
+    }
+    while (A->set[i][k] != '\n') {
+      k++;
+      A->set[i][k] = fgetc(input);
+    }
+    i++;
+  }
+  fclose(input);
+}
